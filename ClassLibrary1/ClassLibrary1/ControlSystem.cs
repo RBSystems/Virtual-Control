2449481bdd6f7b8b8b13ad881e02de98500528de
Dan Savage <dsavage@crestron.com>
2018-08-15 09:47:17 -0400

    Starting Point Project for VC
    Starting point project for creating virtual control projects using references and dependency files from SDK v2.000.3796.12152.

---
+++ ClassLibrary1/ClassLibrary1/ControlSystem.cs
@@ -0,0 +1,213 @@
+using System;
+using System.IO;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;                           // For Basic SIMPL#Pro classes
+using Crestron.SimplSharpPro.CrestronThread;            // For Threading
+using Crestron.SimplSharp.WebScripting;
+
+namespace XiO_Edge_Test_Solution
+{
+    public class Server_Handler : IHttpCwsHandler
+    {
+        private ControlSystem _cs;
+        public Server_Handler(ControlSystem cs)
+        {
+            _cs = cs;
+        }
+
+        void IHttpCwsHandler.ProcessRequest(HttpCwsContext context)
+        {
+            try
+            {
+                // handle requests
+                if (context.Request.RouteData != null)
+                {
+                    switch (context.Request.RouteData.Route.Name.ToUpper())
+                    {
+                        case "ROOMNAME":
+                            context.Response.StatusCode = 200;
+                            context.Response.Write(InitialParametersClass.RoomName, true);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("CWS Handler: {0}", e.Message);
+            }
+
+        }
+    }
+
+    public class ControlSystem : CrestronControlSystem
+    {
+
+        private string myFolder { get { return Crestron.SimplSharp.CrestronIO.Directory.GetApplicationRootDirectory() + "/User/"; } }
+        private HttpCwsServer myServer;
+
+        /// <summary>
+        /// ControlSystem Constructor. Starting point for the SIMPL#Pro program.
+        /// Use the constructor to:
+        /// * Initialize the maximum number of threads (max = 400)
+        /// * Register devices
+        /// * Register event handlers
+        /// * Add Console Commands
+        ///
+        /// Please be aware that the constructor needs to exit quickly; if it doesn't
+        /// exit in time, the SIMPL#Pro program will exit.
+        ///
+        /// You cannot send / receive data in the constructor
+        /// </summary>
+        public ControlSystem()
+            : base()
+        {
+
+
+
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 20;
+
+
+
+                //Subscribe to the controller events (System, Program, and Ethernet)
+                CrestronEnvironment.SystemEventHandler += new SystemEventHandler(ControlSystem_ControllerSystemEventHandler);
+                CrestronEnvironment.ProgramStatusEventHandler += new ProgramStatusEventHandler(ControlSystem_ControllerProgramEventHandler);
+                CrestronEnvironment.EthernetEventHandler += new EthernetEventHandler(ControlSystem_ControllerEthernetEventHandler);
+
+                ErrorLog.Notice("*** : Constructor");
+                ErrorLog.Notice("*** : Current Time is {0}", DateTime.Now);
+                myServer = new HttpCwsServer("api");
+                myServer.Routes.Add(new HttpCwsRoute("roomname") { Name = "ROOMNAME" });
+                myServer.HttpRequestHandler = new Server_Handler(this);
+                myServer.Register();
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in the constructor: {0}", e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// InitializeSystem - this method gets called after the constructor
+        /// has finished.
+        ///
+        /// Use InitializeSystem to:
+        /// * Start threads
+        /// * Configure ports, such as serial and verisports
+        /// * Start and initialize socket connections
+        /// Send initial device configurations
+        ///
+        /// Please be aware that InitializeSystem needs to exit quickly also;
+        /// if it doesn't exit in time, the SIMPL#Pro program will exit.
+        /// </summary>
+        public override void InitializeSystem()
+        {
+            try
+            {
+                ErrorLog.Notice("*** : InitializeSystem");
+                using (FileStream myStream = File.Create(myFolder + "test.txt"))
+                {
+                    byte[] whatToWrite = System.Text.Encoding.ASCII.GetBytes(String.Format("Program Running Name: {0}", InitialParametersClass.RoomName));
+                    myStream.Write(whatToWrite,0,whatToWrite.Length);
+                    myStream.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Event Handler for Ethernet events: Link Up and Link Down.
+        /// Use these events to close / re-open sockets, etc.
+        /// </summary>
+        /// <param name="ethernetEventArgs">This parameter holds the values
+        /// such as whether it's a Link Up or Link Down event. It will also indicate
+        /// wich Ethernet adapter this event belongs to.
+        /// </param>
+        void ControlSystem_ControllerEthernetEventHandler(EthernetEventArgs ethernetEventArgs)
+        {
+            switch (ethernetEventArgs.EthernetEventType)
+            {//Determine the event type Link Up or Link Down
+                case (eEthernetEventType.LinkDown):
+                    //Next need to determine which adapter the event is for.
+                    //LAN is the adapter is the port connected to external networks.
+                    if (ethernetEventArgs.EthernetAdapter == EthernetAdapterType.EthernetLANAdapter)
+                    {
+                        //
+                    }
+                    break;
+                case (eEthernetEventType.LinkUp):
+                    if (ethernetEventArgs.EthernetAdapter == EthernetAdapterType.EthernetLANAdapter)
+                    {
+
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Event Handler for Programmatic events: Stop, Pause, Resume.
+        /// Use this event to clean up when a program is stopping, pausing, and resuming.
+        /// This event only applies to this SIMPL#Pro program, it doesn't receive events
+        /// for other programs stopping
+        /// </summary>
+        /// <param name="programStatusEventType"></param>
+        void ControlSystem_ControllerProgramEventHandler(eProgramStatusEventType programStatusEventType)
+        {
+            switch (programStatusEventType)
+            {
+                case (eProgramStatusEventType.Paused):
+                    //The program has been paused.  Pause all user threads/timers as needed.
+                    ErrorLog.Notice("*** : Pause");
+                    break;
+                case (eProgramStatusEventType.Resumed):
+                    //The program has been resumed. Resume all the user threads/timers as needed.
+                    ErrorLog.Notice("*** : Resumed");
+                    break;
+                case (eProgramStatusEventType.Stopping):
+                    //The program has been stopped.
+                    //Close all threads.
+                    //Shutdown all Client/Servers in the system.
+                    //General cleanup.
+                    //Unsubscribe to all System Monitor events
+                    ErrorLog.Notice("*** : Stopping");
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// Event Handler for system events, Disk Inserted/Ejected, and Reboot
+        /// Use this event to clean up when someone types in reboot, or when your SD /USB
+        /// removable media is ejected / re-inserted.
+        /// </summary>
+        /// <param name="systemEventType"></param>
+        void ControlSystem_ControllerSystemEventHandler(eSystemEventType systemEventType)
+        {
+            switch (systemEventType)
+            {
+                case (eSystemEventType.DiskInserted):
+                    //Removable media was detected on the system
+                    ErrorLog.Notice("*** : Disk Inserted");
+                    break;
+                case (eSystemEventType.DiskRemoved):
+                    //Removable media was detached from the system
+                    ErrorLog.Notice("*** : Disk Removed");
+                    break;
+                case (eSystemEventType.Rebooting):
+                    //The system is rebooting.
+                    //Very limited time to preform clean up and save any settings to disk.
+                    ErrorLog.Notice("*** : Rebooting");
+                    break;
+            }
+
+        }
+    }
+}
